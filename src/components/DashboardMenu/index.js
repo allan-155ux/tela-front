ed160e56a712fca063037c329e4f63b021e86647
allan.155ux <allanmg155@gmail.com>
2024-03-25 20:27:09 -0300

    second commit

--- src/components/DashboardMenu/index.js
+++ src/components/DashboardMenu/index.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './styles.css'
 
 import { Link } from 'react-router-dom'
 
-function DashBoardMenu({ menuIsOpen, setMenuIsOpen }) {
+function DashBoardMenu() {
+
+  const [menuIsOpen, setMenuIsOpen] = useState(true)
 
   return (
     <div>
-      <div className='dashboard-menu'>
+      {!menuIsOpen &&
+        <div onClick={()=>setMenuIsOpen(!menuIsOpen)} className='dashboard-hamburger'>
+          Menu
+        </div>
+      }
+      <div className={menuIsOpen ? 'dashboard-menu' : 'dashboard-desatived-menu'}>
+        {menuIsOpen &&
+          <div onClick={()=>setMenuIsOpen(!menuIsOpen)}>
+            close
+          </div>
+        }
         <h1>Dashboard</h1>
         <ul>
           <Link to='products'><li>Produtos</li></Link>
